3903badf3dc101354aaefb0681100faeaa9fc208
EmmaD9 <99094133+EmmaD9@users.noreply.github.com>
2023-10-26 12:21:23 -0400

    started

---
+++ FallSemesterMealPlanBudgeter/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace FallSemesterMealPlanBudgeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //meal plan types
+            bool roar = false;
+            bool roarPlus = false;
+
+            //get user input
+            string currentDate = DateTime.Now.ToString();
+
+            Console.WriteLine("What is your current meal plan balance?");
+            double balance = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Do you have ROAR or ROAR PLUS as your meal plan type?");
+            string mealPlan = Console.ReadLine().ToUpper().Trim();
+
+            while (!roar && !roarPlus)
+            {
+                if (mealPlan == "ROAR")
+                {
+                    roar = true;
+                    mealPlan = "roar";
+                }
+                else if (mealPlan == "ROARPLUS")
+                {
+                    roarPlus = true;
+                    mealPlan = "roar plus";
+                }
+                else
+                {
+                    Console.WriteLine("Meal plan type not recognized, try again please.");
+                    Console.WriteLine("\tROAR or ROAR PLUS");
+                    mealPlan = Console.ReadLine().ToUpper().Trim();
+                }
+            }
+            
+
+            Console.WriteLine("Today, {0}, you have a balance of {1} for your {2} meal plan.", currentDate, balance, mealPlan);
+        }
+    }
+}
