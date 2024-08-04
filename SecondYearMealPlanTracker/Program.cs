62fe000a4d3de8545ac21401223e8b1a958516ce
EmmaD9 <maduprey1@gmail.com>
2024-08-04 14:28:06 -0400

    initial commit

---
+++ SecondYearMealPlanTracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SecondYearMealPlanTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
